7f4877204429fe15db3ce31fad4d2d356220c9a1
pjlasl <pablleaf@gmail.com>
2020-02-23 10:53:44 -0600

    Initial Commit

---
+++ src/main/java/net/corail/cutter/Addon.java
@@ -0,0 +1,14 @@
+package net.corail.cutter;
+
+import net.fabricmc.api.ModInitializer;
+
+
+public class Addon implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
