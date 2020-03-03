ef82ed7fec0be23e3423ea04e5c18d9f164562b0
pjlasl <pablleaf@gmail.com>
2020-03-02 21:08:50 -0600

    added blockus

--- src/main/resources/data/libcd/tweakers/adorn.js
+++ src/main/resources/data/libcd/tweakers/adorn.js
@@ -1,31 +1,30 @@
  var RecipeTweaker = libcd.require("libcd.recipe.RecipeTweaker");
  var TweakerUtils = libcd.require("libcd.util.TweakerUtils");
-// // This removes Minecraft Vanilla Recipes
-
-log.info('Doing some cleanup on stonecutting recipes if necessary...')
 
 /* This following removes the adorn items from the stonecutter */
-
-var woodtypes = ['acacia', 'birch', 'dark_oak', 'jungle', 'oak', 'spruce', 
+var modId = 'adorn';
+var mod_woods = ['acacia', 'birch', 'dark_oak', 'jungle', 'oak', 'spruce', 
     'terrestria_cypress',  'terrestria_hemlock',  'terrestria_japanese_maple',
     'terrestria_rainbow_eucalyptus',  'terrestria_redwood',  'terrestria_rubber',
     'terrestria_sakura', 'terrestria_willow', 'traverse_fir'];
 
-var adorn_items = ['platform', 'post', 'step'];
+var mod_items = ['platform', 'post', 'step'];
 
-top: for (var i = 0; i <= woodtypes.length - 1; i++) {
-    for (var j = 0; j <= adorn_items.length - 1; j++) {
-        var item = woodtypes[i] + '_' + adorn_items[j];
-        var itemFound = TweakerUtils.getItem('adorn:' + item)
+log.info('Checking for ' + modId + ' mod...')
+
+top: for (var i = 0; i <= mod_woods.length - 1; i++) {
+    for (var j = 0; j <= mod_items.length - 1; j++) {
+        var item = mod_woods[i] + '_' + mod_items[j];
+        var itemFound = TweakerUtils.getItem(modId + ':' + item);
 
         if (itemFound.toString().indexOf('air') >= 0) {
-            log.info('Adorn not found. Moving on...');
+            log.info(modId + ' not found. Moving on...');
             break top;
         }
 
         if (itemFound.toString() === item) {
             log.info(item + ' found. Removing stonecutting recipe');
-            RecipeTweaker.removeRecipesFor('adorn:' + item, "minecraft:stonecutting");
+            RecipeTweaker.removeRecipesFor(modId + ':' + item, "minecraft:stonecutting");
         } else {
             log.info(item + ' not found. Moving on...');
             continue;
@@ -35,4 +34,4 @@ top: for (var i = 0; i <= woodtypes.length - 1; i++) {
     
 }
 
-log.info('Cleanup complete...')
+log.info(modId + ' setup complete...');
